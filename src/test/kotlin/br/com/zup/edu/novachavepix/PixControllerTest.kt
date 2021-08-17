2e0e7dfb83b0c55532e511b572cd1cacb374b4ec
Hugo Oliveira Soares <hugo.soares@zup.com.br>
2021-08-17 16:53:15 -0300

    Criando Testes para a funcionalidade de criação da chave pix

---
+++ src/test/kotlin/br/com/zup/edu/novachavepix/PixControllerTest.kt
@@ -0,0 +1,103 @@
+package br.com.zup.edu.novachavepix
+
+import br.com.zup.edu.ChavePixRequest
+import br.com.zup.edu.KeyManagerGrpcServiceGrpc
+import io.grpc.ManagedChannel
+import io.grpc.Status
+import io.grpc.StatusRuntimeException
+import io.micronaut.context.annotation.Factory
+import io.micronaut.grpc.annotation.GrpcChannel
+import io.micronaut.grpc.server.GrpcServerChannel
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.util.*
+import javax.inject.Singleton
+
+@MicronautTest(transactional = false)
+internal class PixControllerTest(
+    val repository: ChavePixRepository,
+    val grpcClient: KeyManagerGrpcServiceGrpc.KeyManagerGrpcServiceBlockingStub
+) {
+
+    @BeforeEach
+    internal fun setUp() {
+        repository.deleteAll()
+    }
+
+    @Test
+    fun `deve adicionar nova chave pix`() {
+
+        val response = grpcClient.registraChavePix(
+            ChavePixRequest.newBuilder()
+                .setIdentificador("c56dfef4-7901-44fb-84e2-a2cefb157890")
+                .setTipoChave(ChavePixRequest.TipoChave.CPF)
+                .setChave("70306444682")
+                .setTipoConta(ChavePixRequest.TipoConta.Conta_Corrente)
+                .build()
+        )
+
+        with(response) {
+            println("Id do Pix: $pixId")
+            assertNotNull(pixId)
+            assertTrue(repository.existsById(pixId))
+        }
+
+    }
+
+    @Test
+    fun `nao deve cadastrar uma nova chave pix ja existente`(){
+
+        val chaveExistente = repository.save(
+            ChavePix(
+                UUID.randomUUID().toString(),
+                ChavePixRequest.TipoChave.CPF,
+                "12419962028",
+                ChavePixRequest.TipoConta.Conta_Corrente
+            )
+        )
+
+        val exception = assertThrows<StatusRuntimeException> {
+            grpcClient.registraChavePix(
+                ChavePixRequest.newBuilder()
+                    .setIdentificador(chaveExistente.identificadorCliente)
+                    .setTipoChave(chaveExistente.tipoChave)
+                    .setChave(chaveExistente.chave)
+                    .setTipoConta(chaveExistente.tipoConta)
+                    .build()
+            )
+        }
+
+        assertEquals(Status.ALREADY_EXISTS.code, exception.status.code)
+
+    }
+
+    @Test
+    fun `nao deve cadastrar uma nova chave pix com dados invalidos`(){
+
+        val exception = assertThrows<StatusRuntimeException> {
+            grpcClient.registraChavePix(
+                ChavePixRequest.newBuilder()
+                    .setTipoChave(ChavePixRequest.TipoChave.CPF)
+                    .setChave(" ")
+                    .setTipoConta(ChavePixRequest.TipoConta.Conta_Corrente)
+                    .build()
+            )
+        }
+
+        assertEquals(Status.INVALID_ARGUMENT.code, exception.status.code)
+
+    }
+
+    @Factory
+    class Clients {
+        @Singleton
+        fun blockingStub(@GrpcChannel(GrpcServerChannel.NAME) channel: ManagedChannel): KeyManagerGrpcServiceGrpc.KeyManagerGrpcServiceBlockingStub? {
+            return KeyManagerGrpcServiceGrpc.newBlockingStub(channel)
+        }
+
+    }
+
+}
